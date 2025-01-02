b018605025f3c5dbd564e2db8ced1a770df95fc7
BonnEsteban <ebonnard3@myges.fr>
2025-01-02 14:00:41 +0100

    add crud sql request

--- connectToDb.py
+++ connectToDb.py
@@ -1,8 +1,11 @@
 import psycopg2
 
-db_conection = psycopg2.connect(database="postgres", user='postgres', password='|)r6>|}ST87B', host="82.67.90.50", port=5432)
-try:
-    cursor = db_conection.cursor()
-    print("connection réussi")
-except:
+def connectdb():
+    db_conection = psycopg2.connect(database="projet_python", user='adminbdd', password='|)r6>|}ST87B', host="82.67.90.50", port=5432)
+    try:
+        cursor = db_conection.cursor()
+        print("connection réussi")
+    except:
      raise ValueError("la connection n'as pas fonctionner")
+    return cursor
+    
